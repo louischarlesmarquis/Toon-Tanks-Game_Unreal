c3fc96f7ea37573d2b6bd3a10210e0b9031b71d1
Louis-Charles Marquis <lcmarquis42@gmail.com>
2023-01-20 10:16:17 -0500

    Add files via upload

---
+++ ToonTanks.Target.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ToonTanksTarget : TargetRules
+{
+	public ToonTanksTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "ToonTanks" } );
+	}
+}
---
+++ ToonTanks/Tower.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void Tick(float DeltaTime) override;
+
+	void HandleDestruction();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	class ATank* Tank;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float FireRange = 300.f;
+
+	FTimerHandle FireRateTimerHandle;
+	float FireRate = 2.f;
+	void CheckFireCondition();
+
+	bool InFireRange();
+};
